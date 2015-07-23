00d95bbdfe809d5e6edcb7e0eab734d7a16272c9
Kevin Scott <kscott@tenable.com>
2015-07-23 13:48:04 -0400

    Add files for heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+/* globals process, __dirname */
+// A little script so we can view files on heroku
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
